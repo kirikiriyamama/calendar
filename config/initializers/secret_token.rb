b6b5c4ef7aa1d3e767db269534fee14807adda40
remain <ponde.ponde.pond@gmail.com>
2013-05-15 23:58:41 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendar::Application.config.secret_token = '7669ee89d5cd98002dd6f8a432de9fb35e5a175cf8997a294c57eb5bcf58acb943fb354f8aa8957c1d9bfe6dd22bf1fd760dc5a28234145b70521cf30a4e28f5'

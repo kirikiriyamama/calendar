7ba5b6af137df9d8bcb0a70a9af541a35df6ea05
remain <ponde.ponde.pond@gmail.com>
2013-05-16 02:18:33 +0900

    add asociation
    schedule has_many tasks

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,4 @@
 class Schedule < ActiveRecord::Base
   attr_accessible :deadline_at, :description, :notes, :summary
+  has_many :tasks
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ActiveRecord::Base
   attr_accessible :complete, :content, :schedule_id
+  belongs_to :schedule
 end

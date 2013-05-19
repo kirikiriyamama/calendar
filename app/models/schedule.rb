8e616ca0d4e6407395d522031c8fa71faf9a11d6
remain <ponde.ponde.pond@gmail.com>
2013-05-19 16:17:45 +0900

    edit app/models/schedule.rb
    add validates

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,11 @@
 class Schedule < ActiveRecord::Base
   attr_accessible :deadline_at, :description, :notes, :summary
+
   has_many :tasks
+  
+  validates :deadline_at,
+    :presence => true,
+  validates :summary,
+    :presence => true,
+    :length => { :maximum => 255, :allow_blank => true }
 end

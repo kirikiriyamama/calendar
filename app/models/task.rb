384cfbe96ef8296101791834091b80227120ef0a
remain <ponde.ponde.pond@gmail.com>
2013-05-19 16:18:37 +0900

    edit app/models/task.rb
    add validates

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ActiveRecord::Base
   attr_accessible :complete, :content, :schedule_id
+
   belongs_to :schedule
+
+  validate :schedule_id,
+    :presence => true
+  validate :content,
+    :presence => true
+    :length => { :maximum => 255, :allow_blank => true }
+  validate :complete,
+    :presence => true
 end

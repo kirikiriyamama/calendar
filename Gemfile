fbd236a5c510e9328ada778228a39461ac1cc2a2
remain <ponde.ponde.ponde@gmail.com>
2014-01-18 11:16:49 +0900

    update rails

--- Gemfile
+++ Gemfile
@@ -1,29 +1,30 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'rails', '3.2.13'
+gem "rails", "4.0.2"
 
-gem 'sqlite3'
+gem "sqlite3", "~> 1.3"
 
 group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
-  gem 'haml-rails'
+  gem "sass-rails"
+  gem "coffee-rails"
+  gem "haml-rails"
 
-  gem 'therubyracer'
+  # gem "therubyracer", "~> 0.11"
 
-  gem 'uglifier', '>= 1.0.3'
+  gem "uglifier"
 end
 
 group :development do
-  gem 'erb2haml'
+  gem "erb2haml", "~> 0.1"
 end
 
 group :test do
-  gem 'rspec-rails'
+  gem "rspec-rails", "~> 2.13"
 end
 
-gem 'jquery-rails'
-gem 'jquery-ui-rails'
-gem 'bootstrap-sass'
-gem 'simple_form'
-gem 'fullcalendar-rails'
+gem "jquery-rails"
+gem "jquery-ui-rails"
+gem "bootstrap-sass", "~> 2.3"
+gem "simple_form"
+gem "fullcalendar-rails", "~> 1.5"
+gem "nested_form", "~> 0.3"
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Calendar::Application.config.secret_token = '7669ee89d5cd98002dd6f8a432de9fb35e5a175cf8997a294c57eb5bcf58acb943fb354f8aa8957c1d9bfe6dd22bf1fd760dc5a28234145b70521cf30a4e28f5'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Calendar::Application.config.secret_key_base = '0c7eed015a2364d7894e527b725fb817b4b786dc5539b4854cc034955f70481f568a9ea8a52a9ddfa138a8ec66afec586bf0eedb323a86cd31233f35816909de'

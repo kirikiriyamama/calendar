007fe31b6049b90087af1f85f551bf7655aa230d
remain <ponde.ponde.ponde@gmail.com>
2014-01-20 10:19:14 +0900

    cosmetic change

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,8 +2,7 @@ class Schedule < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   accepts_nested_attributes_for :tasks, allow_destroy: true
 
-  validates :deadline_at,
-    :presence => true
+  validates_presence_of :deadline_at
   validates :summary,
     :presence => true,
     :length => { :maximum => 255, :allow_blank => true }
